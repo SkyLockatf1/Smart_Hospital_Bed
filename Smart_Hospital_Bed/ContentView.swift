f47100901e1753e808f9d5fbcb2abfe1916e0445
SkyLock <g2487701324@gmail.com>
2022-04-19 23:29:05 +0800

    first gesture

--- Smart_Hospital_Bed/ContentView.swift
+++ Smart_Hospital_Bed/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TabView{
-            Homeview()
+            Home()
+                .tabItem{
+                    Label("Home",systemImage: "house")
+                }
             
         }
     }
