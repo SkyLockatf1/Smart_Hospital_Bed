57b2d9f9a271cfa7eb8fd3c79d14ce92b8bf5438
SkyLock <g2487701324@gmail.com>
2022-04-19 22:05:08 +0800

    first

---
+++ Smart_Hospital_Bed/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  Smart_Hospital_Bed
+//
+//  Created by Sky Lock on 19/4/2022.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
