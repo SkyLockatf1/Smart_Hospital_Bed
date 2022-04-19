888ea5d12a4a32aed6eea27e3d2823b0161f49b2
SkyLock <g2487701324@gmail.com>
2022-04-19 21:57:16 +0800

    Initial Commit

---
+++ Smart_Hospital_Bed/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Smart_Hospital_Bed
+//
+//  Created by Sky Lock on 19/4/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Smart_Hospital_BedUITests/Smart_Hospital_BedUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Smart_Hospital_BedUITestsLaunchTests.swift
+//  Smart_Hospital_BedUITests
+//
+//  Created by Sky Lock on 19/4/2022.
+//
+
+import XCTest
+
+class Smart_Hospital_BedUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
