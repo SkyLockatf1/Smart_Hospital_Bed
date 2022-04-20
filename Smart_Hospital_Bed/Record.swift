387141762ca778aa07781bdd227144d86a6637c5
SkyLock <g2487701324@gmail.com>
2022-04-20 10:56:43 +0800

    update

---
+++ Smart_Hospital_Bed/Record.swift
@@ -0,0 +1,20 @@
+//
+//  Record.swift
+//  Smart_Hospital_Bed
+//
+//  Created by Sky Lock on 20/4/2022.
+//
+
+import SwiftUI
+
+struct Record: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Record_Previews: PreviewProvider {
+    static var previews: some View {
+        Record()
+    }
+}
