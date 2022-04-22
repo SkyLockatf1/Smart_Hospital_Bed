c5c82ec2459ee21f6b7e67b685a774b57a7da0de
SkyLock <g2487701324@gmail.com>
2022-04-23 00:27:48 +0800

    record update

--- Smart_Hospital_Bed/Record.swift
+++ Smart_Hospital_Bed/Record.swift
@@ -6,16 +6,49 @@
 //
 
 import SwiftUI
-import AVFoundation
+import AVKit
 
-class RecorderController: AVAudioRecorderDelegate{
-      struct Recorder: View{
+struct Recorder: View{
+    @State var record = false
+    @State var session : AVAudioSession!
+    @State var recorder: AVAudioRecorder!
+    @State var alert = false
     
       var body: some View {
-          Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+          VStack{
+              Button(action:{
+                  self.record.toggle()
+              }) {
+                  ZStack{
+                      Circle()
+                          .fill(Color.red)
+                          .frame(width: 70, height: 70)
+                      if self.record{
+                          Circle()
+                              .stroke(Color.white,lineWidth: 6)
+                              .frame(width: 85, height: 85)
+                      }
+                  }
+              }
+              .padding(.vertical, 25)
+          }
+          .onAppear{
+              do{
+                  self.session = AVAudioSession.sharedInstance()
+                  try self.session.setCategory(.playAndRecord)
+                  self.session.requestRecordPermission{(status) in
+                      if !status{
+                          self.alert.toggle()
+                      }
+                  }
+              }
+              catch{
+                  print(error.localizedDescription)
+                  
+              }
+          }
       }
     }
-}
 
 struct Record: View {
     var body: some View {
@@ -38,5 +71,6 @@ struct Record: View {
 struct Record_Previews: PreviewProvider {
     static var previews: some View {
         Record()
+        Recorder()
     }
 }
