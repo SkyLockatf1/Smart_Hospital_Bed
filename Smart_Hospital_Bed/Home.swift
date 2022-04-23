ba8e5422b558d8125a91784efb721d4a38bbc900
SkyLock <g2487701324@gmail.com>
2022-04-24 01:49:05 +0800

    update

--- Smart_Hospital_Bed/Home.swift
+++ Smart_Hospital_Bed/Home.swift
@@ -7,14 +7,41 @@
 
 import SwiftUI
 
-struct SwiftUIView: View {
+
+
+struct Home: View {
+    @State private var showingAlert = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("Welcome to Smart Hospital Bed!")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(Color.purple)
+                .multilineTextAlignment(.center)
+            Image("user")
+            Text("User")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.purple)
+            Button(action:{showingAlert = true}){
+                    Text("Emergency Call")
+                        .fontWeight(.bold)
+                        .background(Color.red)
+                        .font(.largeTitle)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(10)
+                        .padding(.all)
+                    
+                }
+                .alert("Successfully Call Nurse", isPresented: $showingAlert) {
+                        Button("OK", role: .cancel) { }
+                }
+        }
     }
 }
 
-struct SwiftUIView_Previews: PreviewProvider {
+struct Home_Previews: PreviewProvider {
     static var previews: some View {
-        SwiftUIView()
+        Home()
     }
 }
